498144607f8726f00034238beae947b23c7ab7c3
Kevin Flathers <flatherskevin@gmail.com>
2020-03-23 18:11:53 -0400

    continuing tutorial

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,29 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  for side in [a, b, c]
+    if side <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    end
+  end
+  if a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError
+  end
+
+  count = 0
+  count += 1 if a == b
+  count += 1 if a == c
+  count += 1 if b == c
+  case count
+  when 0
+    :scalene
+  when 1
+    :isosceles
+  when 2..3
+    :equilateral
+  else
+    raise TriangleError
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
